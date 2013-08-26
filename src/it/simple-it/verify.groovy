c541f71d8d8a2067273f358cf32dcc7890af7df2
Wolf480pl <wolf480@interia.pl>
2013-08-26 14:17:39 +0200

    Added some templates from trove4j to the integration test, and made the verify script check if the sources are generated.

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -1,3 +1,17 @@
-File touchFile = new File( basedir, "target/touch.txt" );
+File tcols = new File( basedir, "target/generated-source/gnu/trove/TCollections.java" );
+File tbytecol = new File( basedir, "target/generated-source/gnu/trove/TByteCollection.java" );
+File tcharcol = new File( basedir, "target/generated-source/gnu/trove/TCharCollection.java" );
+File tdblcol = new File( basedir, "target/generated-source/gnu/trove/TDoubleCollection.java" );
+File tfltcol = new File( basedir, "target/generated-source/gnu/trove/TFloatCollection.java" );
+File tintcol = new File( basedir, "target/generated-source/gnu/trove/TIntCollection.java" );
+File tlongcol = new File( basedir, "target/generated-source/gnu/trove/TLongCollection.java" );
+File tshortcol = new File( basedir, "target/generated-source/gnu/trove/TShortCollection.java" );
 
-assert touchFile.isFile()
+assert tcols.isFile();
+assert tbytecol.isFile();
+assert tcharcol.isFile();
+assert tdblcol.isFile();
+assert tfltcol.isFile();
+assert tintcol.isFile();
+assert tlongcol.isFile();
+assert tshortcol.isFile();
