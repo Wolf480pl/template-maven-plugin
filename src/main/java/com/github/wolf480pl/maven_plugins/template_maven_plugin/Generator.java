293157a1bbc9573813a2b5056c6780c3e71b0638
Wolf480pl <wolf480@interia.pl>
2013-08-26 14:02:13 +0200

    Set up automatic license headers and other stuff in pom.xml

--- src/main/java/com/github/wolf480pl/maven_plugins/template_maven_plugin/Generator.java
+++ src/main/java/com/github/wolf480pl/maven_plugins/template_maven_plugin/Generator.java
@@ -1,4 +1,23 @@
 /*
+ * This file is part of Maven Plugin For Trove Templates.
+ *
+ * Copyright (c) 2013 Wolf480pl <wolf480@interia.pl>
+ * Maven Plugin For Trove Templates is licensed under the GNU Lesser General Public License.
+ *
+ * Maven Plugin For Trove Templates is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Maven Plugin For Trove Templates is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
  * ///////////////////////////////////////////////////////////////////////////////
  * // Copyright (c) 2009, Rob Eden All Rights Reserved.
  * // Copyright (c) 2009, Jeff Randall All Rights Reserved.
--- src/main/java/com/github/wolf480pl/maven_plugins/template_maven_plugin/StreamLog.java
+++ src/main/java/com/github/wolf480pl/maven_plugins/template_maven_plugin/StreamLog.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Maven Plugin For Trove Templates.
+ *
+ * Copyright (c) 2013 Wolf480pl <wolf480@interia.pl>
+ * Maven Plugin For Trove Templates is licensed under the GNU Lesser General Public License.
+ *
+ * Maven Plugin For Trove Templates is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Maven Plugin For Trove Templates is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.wolf480pl.maven_plugins.template_maven_plugin;
 
 import java.io.PrintStream;
