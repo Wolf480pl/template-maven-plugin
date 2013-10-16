db3ddfd8b1039714b49abcd65baceda136a4532f
Wolf480pl <wolf480@interia.pl>
2013-10-16 21:41:18 +0200

    Set a proper name and description for the project and expand the readme.

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-template-maven-plugin
+Template Maven Plugin
 =====================
 
-A maven plugin wrapper for trove4j template generator
+A maven plugin based on trove4j's source generator. Replicates each template file for every configured primitive and replaces placeholders. It's kinda like compile-time generics for primitive types.
+
+Copyright (c) 2013, Wolf480pl <<wolf480@interia.pl>>
+
+[![Build Status](https://travis-ci.org/Wolf480pl/template-maven-plugin.png?branch=master)](https://travis-ci.org/Wolf480pl/template-maven-plugin)
+
+Source
+------
+The latest and greatest source can be found on [GitHub].  
+
+License
+-------
+Template Maven Plugin is licensed under the [GNU Lesser General Public License Version 3][License].
+
+Compiling
+---------
+Template Maven Plugin uses Maven to handle its dependencies.
+
+* Install [Maven 2 or 3](http://maven.apache.org/download.html)
+* Checkout this repo and run: `mvn clean install`
+
+[GitHub]: https://github.com/Wolf480pl/template-maven-plugin
+[License]: http://www.gnu.org/licenses/lgpl.html
--- src/main/java/com/github/wolf480pl/maven_plugins/template_maven_plugin/GenerateMojo.java
+++ src/main/java/com/github/wolf480pl/maven_plugins/template_maven_plugin/GenerateMojo.java
@@ -1,15 +1,15 @@
 /*
- * This file is part of Maven Plugin For Trove Templates.
+ * This file is part of Template Maven Plugin.
  *
  * Copyright (c) 2013 Wolf480pl <wolf480@interia.pl>
- * Maven Plugin For Trove Templates is licensed under the GNU Lesser General Public License.
+ * Template Maven Plugin is licensed under the GNU Lesser General Public License.
  *
- * Maven Plugin For Trove Templates is free software: you can redistribute it and/or modify
+ * Template Maven Plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Maven Plugin For Trove Templates is distributed in the hope that it will be useful,
+ * Template Maven Plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
